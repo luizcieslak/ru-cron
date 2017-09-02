2c15901fcf9d3745ba62a78c7b92765d022d224d
luizcieslak <cieslakluiz@gmail.com>
2017-09-02 18:22:50 -0300

    fixed typo on http address

--- clock.py
+++ clock.py
@@ -10,9 +10,8 @@ cron_key = F.read()
 @sched.scheduled_job('interval', minutes=3)
 def timed_job():
     print('This job is run every three minutes.')
-        # Run the http request
-    payload={'key': cron_key, 'rid': 0}
-    res = requests.get('http://localhost:5000/unespru/us-central1/test', params=payload)
+    # Run the http request
+    res = requests.get('https://us-central1-unespru.cloudfunctions.net/test', params=payload)
     print(res.text)
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=13)
@@ -20,7 +19,7 @@ def scheduled_job():
     print('This job is run every weekday at 1pm.')
     # Run the http request
     payload={'key': cron_key, 'rid': 0}
-    res = requests.get('http://localhost:5000/unespru/us-central1/queueCleanup', params=payload)
+    res = requests.get('https://us-central1-unespru.cloudfunctions.net/queueCleanup', json=payload)
     print(res.text)
 
 sched.start()
