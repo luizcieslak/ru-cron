6219de5937fb380cf646307eca4063bb68c4d852
luizcieslak <cieslakluiz@gmail.com>
2017-09-02 19:10:47 -0300

    replaced test function with a new cron function

--- clock.py
+++ clock.py
@@ -7,9 +7,9 @@ if __name__ == '__main__':
 F = open('cron.key.txt','r')
 cron_key = F.read()
 
-@sched.scheduled_job('interval', minutes=3)
-def timed_job():
-    print('This job is run every three minutes.')
+# @sched.scheduled_job('interval', minutes=3)
+# def timed_job():
+#     print('This job is run every three minutes.')
     # Run the http request
     # res = requests.get('https://us-central1-unespru.cloudfunctions.net/test')
     # print(res.text)
@@ -22,5 +22,12 @@ def scheduled_job():
     res = requests.get('https://us-central1-unespru.cloudfunctions.net/queueCleanup', json=payload)
     print(res.text)
 
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=10)
+def scheduled_job():
+    print('This job is run every weekday at 10am.')
+    # Run the http request
+    payload={'key': cron_key}
+    res = requests.get('https://us-central1-unespru.cloudfunctions.net/oneHourBefore', json=payload)
+    print(res.text)
 sched.start()
 
